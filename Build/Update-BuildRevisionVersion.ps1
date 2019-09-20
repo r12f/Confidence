e12060d5ac4056b07f8e598a5a2a95a6461f993f
r12f <r12f.code@gmail.com>
2019-09-20 08:56:36 -0700

    Update version number generation.

--- Build/Update-BuildRevisionVersion.ps1
+++ Build/Update-BuildRevisionVersion.ps1
@@ -1,3 +1,8 @@
+param (
+    [Parameter(Mandatory = $true, Position = 0)]
+    [string] $Version
+)
+
 function Update-BuildRevisionVersion()
 {
     $buildConfigFolder = $PSScriptRoot
@@ -5,24 +10,11 @@ function Update-BuildRevisionVersion()
     $versionFilePath = "$buildConfigFolder\config\version.txt"
     Write-Host "Version file path: $versionFilePath"
 
-    $versionFileContent = Get-Content $versionFilePath
-    $versionFileContent = $versionFileContent.Trim("\r\n ")
-    Write-Host "Current version read: $versionFileContent"
-
-    $versionParts = $versionFileContent.Split('.')
-    $versionParts[2] = ([System.Convert]::ToInt32($versionParts[2]) + 1).ToString();
-    $newVersion = $versionParts -join "."
-
+    $newVersion = "$Version.0"
     Write-Host "Updating version to: $newVersion"
+
     $encoding = New-Object System.Text.UTF8Encoding $False
     [System.IO.File]::WriteAllLines($versionFilePath, $newVersion, $encoding)
-
-    cd "$buildConfigFolder\.."
-    & git config user.name "r12f"
-    & git config user.email "r12f.code@gmail.com"
-    & git add . 
-    & git commit -m "Increase version number to $newVersion."
-    & git push
 }
 
 Update-BuildRevisionVersion
