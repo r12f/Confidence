66c6531ba23ff048f830f96563892de7f8b4119a
r12f <r12f.code@gmail.com>
2019-09-20 23:24:05 -0700

    Adding sample project.

---
+++ Confidence.Samples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Confidence.Samples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
