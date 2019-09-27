2d6686519a8ab72d75feabb79b43b6c76a3efef1
r12f <r12f.code@gmail.com>
2019-09-26 23:24:28 -0700

    Add String Contains/DoesNotContain validations.

--- Confidence/Validations/StringValidateTargetExtensions.cs
+++ Confidence/Validations/StringValidateTargetExtensions.cs
@@ -329,5 +329,45 @@ namespace Confidence
 
             return target;
         }
+
+        /// <summary>
+        /// Validate if target contains a specific value.
+        /// </summary>
+        /// <param name="target">Validate target.</param>
+        /// <param name="valueToCompare">Value to compare.</param>
+        /// <param name="getErrorMessage">Custom error message.</param>
+        /// <param name="stringComparison">String comparison.</param>
+        /// <returns>The same validate target as passed in.</returns>
+        [ValidationMethod(ValidationTargetTypes.String, ValidationMethodTypes.Comparison)]
+        [DebuggerStepThrough]
+        public static ValidateTarget<string> Contains([ValidatedNotNull] this ValidateTarget<string> target, string valueToCompare, Func<string> getErrorMessage = null, StringComparison stringComparison = StringComparison.Ordinal)
+        {
+            if (target.Value == null || target.Value.IndexOf(valueToCompare, stringComparison) < 0)
+            {
+                ExceptionFactory.ThrowException(target.Traits.GenericFailureExceptionType, getErrorMessage != null ? getErrorMessage.Invoke() : ErrorMessageFactory.ShouldContain(target, valueToCompare));
+            }
+
+            return target;
+        }
+
+        /// <summary>
+        /// Validate if target doesn't contain a specific value.
+        /// </summary>
+        /// <param name="target">Validate target.</param>
+        /// <param name="valueToCompare">Value to compare.</param>
+        /// <param name="getErrorMessage">Custom error message.</param>
+        /// <param name="stringComparison">String comparison.</param>
+        /// <returns>The same validate target as passed in.</returns>
+        [ValidationMethod(ValidationTargetTypes.String, ValidationMethodTypes.Comparison)]
+        [DebuggerStepThrough]
+        public static ValidateTarget<string> DoesNotContain([ValidatedNotNull] this ValidateTarget<string> target, string valueToCompare, Func<string> getErrorMessage = null, StringComparison stringComparison = StringComparison.Ordinal)
+        {
+            if (target.Value != null && target.Value.IndexOf(valueToCompare, stringComparison) >= 0)
+            {
+                ExceptionFactory.ThrowException(target.Traits.GenericFailureExceptionType, getErrorMessage != null ? getErrorMessage.Invoke() : ErrorMessageFactory.ShouldNotContain(target, valueToCompare));
+            }
+
+            return target;
+        }
     }
 }
